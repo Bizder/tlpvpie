3011302875148996b316504ead2021bb4341510f
Bizder <gergely.tokodi@gmail.com>
2018-02-14 21:21:41 +0100

    docker command, cuz i'm lazy

---
+++ dbat.bat
@@ -0,0 +1,1 @@
+docker run -d -it --mount type=bind,source=c:/Users/tokodi/STUDY/ns3-container/simpletcpclient/,target=/home/tokodi/workspace/ns-allinone-3.27/ns-3.27/scratch/  --mount type=bind,source=c:/Users/tokodi/STUDY/ns3-container/output/,target=/home/tokodi/workspace/ns-allinone-3.27/ns-3.27/output/ ns3 /bin/bash
