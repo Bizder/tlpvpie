8fb21d9c0262e2c5b3b8236963f7ba9fe555bc4b
Bizder <gergely.tokodi@gmail.com>
2018-02-18 11:51:00 +0100

    final file created

--- dbat.bat
+++ dbat.bat
@@ -1,1 +1,1 @@
-docker run -d -it --mount type=bind,source=c:/Users/tokodi/STUDY/ns3-container/simpletcpclient/,target=/home/tokodi/workspace/ns-allinone-3.27/ns-3.27/scratch/  --mount type=bind,source=c:/Users/tokodi/STUDY/ns3-container/output/,target=/home/tokodi/workspace/ns-allinone-3.27/ns-3.27/output/ ns3 /bin/bash
+docker run -d -it --mount type=bind,source=,target=/home/tokodi/workspace/ns-allinone-3.27/ns-3.27/scratch/  --mount type=bind,source=c:/Users/tokodi/STUDY/ns3-container/output/,target=/home/tokodi/workspace/ns-allinone-3.27/ns-3.27/output/ ns3 /bin/bash
