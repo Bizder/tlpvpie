06d8ba890297109f8301f4a09c42d2dcadf0a4da
Tokodi Gergely <gergely.tokodi@nokia.com>
2018-02-20 17:10:55 +0100

    renaming

--- pvpie.cc
+++ pvpie.cc
@@ -4,27 +4,27 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 
-NS_LOG_COMPONENT_DEFINE ("PIE");
+NS_LOG_COMPONENT_DEFINE ("PVPIE");
 
-class UnlimitedRateApp : public ns3::Application
+class ValuedApp : public ns3::Application
 {
 	public:
-		UnlimitedRateApp();
-		virtual ~ UnlimitedRateApp();
+		ValuedApp();
+		virtual ~ ValuedApp();
 
-		void Setup(ns3::Ptr<ns3::Socket> socket, ns3::Address address, uint32_t packetSize);
+		void Setup(ns3::Ptr<ns3::Socket> socket, ns3::Address address, uint32_t packetSize, uint8_t packetValue);
 
 	private:
-		// inherited from Application base class.
-		virtual void StartApplication(void); // Called at time specified by Start, inherited
-		virtual void StopApplication(void); // Called at time specified by Stop, inherited
-	
+		virtual void StartApplication(void);
+		virtual void StopApplication(void);
+
 		void SendData();
 
-		ns3::Ptr<ns3::Socket> m_socket;  // Associated socket
-		ns3::Address m_peer;        // Peer address
-		bool m_connected;           // True if connected
-		uint32_t m_packetSize;      // Size of data to send each time
+		ns3::Ptr<ns3::Socket> m_socket;
+		ns3::Address m_peer;
+		bool m_connected;
+		uint32_t m_packetSize;
+		uint8_t m_packetValue;
 
 		void ConnectionSucceeded(ns3::Ptr<ns3::Socket> socket);
 		void ConnectionFailed(ns3::Ptr<ns3::Socket> socket);
@@ -32,47 +32,48 @@ class UnlimitedRateApp : public ns3::Application
 		void Ignore(ns3::Ptr<ns3::Socket> socket);
 };
 
-UnlimitedRateApp::UnlimitedRateApp(): m_socket(0), m_connected(false), m_packetSize(512)
+ValuedApp::ValuedApp(): m_socket(0), m_connected(false), m_packetSize(512), m_packetValue(0)
 {
 }
 
-UnlimitedRateApp::~UnlimitedRateApp()
+ValuedApp::~ValuedApp()
 {
 	m_socket = 0;
 }
 
-void UnlimitedRateApp::Setup(ns3::Ptr<ns3::Socket> socket, ns3::Address address, uint32_t packetSize)
+void ValuedApp::Setup(ns3::Ptr<ns3::Socket> socket, ns3::Address address, uint32_t packetSize, uint8_t packetValue)
 {
 	m_socket = socket;
 	m_peer = address;
 	m_packetSize = packetSize;
+	m_packetValue = packetValue;
 
 	// Fatal error if socket type is not NS3_SOCK_STREAM or NS3_SOCK_SEQPACKET
 	if (m_socket->GetSocketType() != ns3::Socket::NS3_SOCK_STREAM &&
 		m_socket->GetSocketType() != ns3::Socket::NS3_SOCK_SEQPACKET)
 	{
-		
 		NS_FATAL_ERROR
-			("Using UnlimitedRateApp with an incompatible socket type. "
-			 "UnlimitedRateApp requires SOCK_STREAM or SOCK_SEQPACKET. "
+			("Using ValuedApp with an incompatible socket type. "
+			 "ValuedApp requires SOCK_STREAM or SOCK_SEQPACKET. "
 			 "In other words, use TCP instead of UDP.");
 	}
 }
 
-void UnlimitedRateApp::StartApplication()
+void ValuedApp::StartApplication()
 {
 	m_socket->Bind();
 	m_socket->Connect(m_peer);
+	m_socket->SetIpTos(m_packetValue);
 	m_socket->ShutdownRecv();
-	m_socket->SetConnectCallback(ns3::MakeCallback(&UnlimitedRateApp::ConnectionSucceeded, this), ns3::MakeCallback(&UnlimitedRateApp::ConnectionFailed, this));
-	m_socket->SetSendCallback(ns3::MakeCallback(&UnlimitedRateApp::DataSend, this));
+	m_socket->SetConnectCallback(ns3::MakeCallback(&ValuedApp::ConnectionSucceeded, this), ns3::MakeCallback(&ValuedApp::ConnectionFailed, this));
+	m_socket->SetSendCallback(ns3::MakeCallback(&ValuedApp::DataSend, this));
 	if (m_connected)
 	{
 		SendData();
 	}
 }
 
-void UnlimitedRateApp::StopApplication()
+void ValuedApp::StopApplication()
 {
 	if (m_socket != 0) {
 		m_socket->Close();
@@ -80,40 +81,35 @@ void UnlimitedRateApp::StopApplication()
 	}
 }
 
-void UnlimitedRateApp::SendData(void)
+void ValuedApp::SendData(void)
 {
 	// We exit this loop when actual<toSend as the send side
 	// buffer is full. The "DataSend" callback will pop when
 	// some buffer space has freed ip.
 	for (;;) {
 		ns3::Ptr<ns3::Packet> packet = ns3::Create<ns3::Packet>(m_packetSize);
-		// TODO VP-1: set packet
-		ns3::Ipv4Header header;
-		packet->RemoveHeader(header);
-		header.SetDscp(ns3::Ipv4Header::DscpType::DSCP_CS4);
-		packet->AddHeader(header);
 		int actual = m_socket->Send(packet);
-		if ((unsigned) actual != m_packetSize) 
+		if ((unsigned) actual != m_packetSize)
 		{
 			break;
 		}
 	}
 }
 
-void UnlimitedRateApp::ConnectionSucceeded(ns3::Ptr<ns3::Socket> socket)
+void ValuedApp::ConnectionSucceeded(ns3::Ptr<ns3::Socket> socket)
 {
 	m_connected = true;
 	SendData();
 }
 
-void UnlimitedRateApp::ConnectionFailed(ns3::Ptr<ns3::Socket> socket)
+void ValuedApp::ConnectionFailed(ns3::Ptr<ns3::Socket> socket)
 {
 }
 
-void UnlimitedRateApp::DataSend(ns3::Ptr<ns3::Socket>, uint32_t)
+void ValuedApp::DataSend(ns3::Ptr<ns3::Socket>, uint32_t)
 {
 	if (m_connected) {
-		ns3::Simulator::ScheduleNow(&UnlimitedRateApp::SendData, this);
+		ns3::Simulator::ScheduleNow(&ValuedApp::SendData, this);
 	}
 }
 
@@ -122,32 +118,32 @@ int main (int argc, char *argv[])
 	ns3::CommandLine cmd;
 	cmd.Parse (argc, argv);
 
-	int number_of_clients = 3;
 	int port = 9000;
+	float startTime = 0.0;
+	float stopTime = 120.0;
 
-	std::string access_bandwidth = "100Mbps";
-	std::string access_delay = "20ms";
+	int nClients = 3;
+	unsigned int packetSize = 1024;
 
-	std::string bottleneck_bandwidth ="10Mbps";
-	std::string bottleneck_delay = "20ms";
+	std::string accessBandwidth = "100Mbps";
+	std::string accessDelay = "20ms";
+
+	std::string bottleneckBandwidth ="10Mbps";
+	std::string bottleneckDelay = "20ms";
 
 	ns3::Time::SetResolution (ns3::Time::NS);
-	// ns3::LogComponentEnable ("UdpEchoClientApplication", ns3::LOG_LEVEL_INFO);
-	// ns3::LogComponentEnable ("UdpEchoServerApplication", ns3::LOG_LEVEL_INFO);
-	// ns3::LogComponentEnable ("UnlimitedRateApp", ns3::LOG_LEVEL_INFO);
 
-	// // Create link helpers
+	// Create link helpers
 	ns3::PointToPointHelper bottleNeckLink;
-	bottleNeckLink.SetDeviceAttribute("DataRate", ns3::StringValue(bottleneck_bandwidth));
-	bottleNeckLink.SetChannelAttribute("Delay", ns3::StringValue(bottleneck_delay));
+	bottleNeckLink.SetDeviceAttribute("DataRate", ns3::StringValue(bottleneckBandwidth));
+	bottleNeckLink.SetChannelAttribute("Delay", ns3::StringValue(bottleneckDelay));
 
 	ns3::PointToPointHelper leafLink;
-	leafLink.SetDeviceAttribute("DataRate", ns3::StringValue(access_bandwidth));
-	leafLink.SetChannelAttribute("Delay", ns3::StringValue(access_delay));
+	leafLink.SetDeviceAttribute("DataRate", ns3::StringValue(accessBandwidth));
+	leafLink.SetChannelAttribute("Delay", ns3::StringValue(accessDelay));
 
-	// // Creating Nodes
+	// Creating Nodes
 	ns3::NodeContainer routers;
-	// TODO VP-3: add an APP
 	ns3::Ptr<ns3::Node> leftrouter = ns3::CreateObject<ns3::Node>();
 	routers.Add(leftrouter);
 	ns3::Ptr<ns3::Node> rightrouter = ns3::CreateObject<ns3::Node>();
@@ -155,10 +151,10 @@ int main (int argc, char *argv[])
 
 	ns3::NodeContainer leftleaves;
 	ns3::NodeContainer rightleaves;
-	leftleaves.Create(number_of_clients);
-	rightleaves.Create(number_of_clients);
+	leftleaves.Create(nClients);
+	rightleaves.Create(nClients);
 
-	// // Add links to connect Nodes
+	// Add links to connect Nodes
 	ns3::NetDeviceContainer routerdevices = bottleNeckLink.Install(routers);
 
 	ns3::NetDeviceContainer leftrouterdevices;
@@ -166,7 +162,7 @@ int main (int argc, char *argv[])
 	ns3::NetDeviceContainer rightrouterdevices;
 	ns3::NetDeviceContainer rightleafdevices;
 
-	for (int i = 0; i < number_of_clients ; ++i)
+	for (int i = 0; i < nClients ; ++i)
 	{
 		ns3::NetDeviceContainer cleft = leafLink.Install(routers.Get(0), leftleaves.Get(i));
 		leftrouterdevices.Add(cleft.Get(0));
@@ -177,12 +173,13 @@ int main (int argc, char *argv[])
 		rightleafdevices.Add(cright.Get(1));
 	}
 
-	// // Assign IPv4 addresses
+	// Assign IPv4 addresses
 	ns3::InternetStackHelper stack;
 	stack.Install(routers);
 	stack.Install(leftleaves);
 	stack.Install(rightleaves);
 
+	// TODO: Redesign address allocating ( same helper )
 	ns3::Ipv4AddressHelper routerips = ns3::Ipv4AddressHelper("10.3.1.0", "255.255.255.0");
 	ns3::Ipv4AddressHelper leftips   = ns3::Ipv4AddressHelper("10.1.1.0", "255.255.255.0");
 	ns3::Ipv4AddressHelper rightips  = ns3::Ipv4AddressHelper("10.2.1.0", "255.255.255.0");
@@ -195,7 +192,7 @@ int main (int argc, char *argv[])
 
 	routerifs = routerips.Assign(routerdevices);
 
-	for (int i = 0; i < number_of_clients ; ++i )
+	for (int i = 0; i < nClients; ++i )
 	{
 		ns3::NetDeviceContainer ndcleft;
 		ndcleft.Add(leftleafdevices.Get(i));
@@ -219,49 +216,30 @@ int main (int argc, char *argv[])
 	ns3::Address sinkLocalAddress(ns3::InetSocketAddress(ns3::Ipv4Address::GetAny(), port));
 	ns3::PacketSinkHelper TcpPacketSinkHelper("ns3::TcpSocketFactory", sinkLocalAddress);
 
-	for ( int i = 0; i < number_of_clients; ++i)
+	for ( int i = 0; i < nClients; ++i)
 	{
 		ns3::Ptr<ns3::Socket> sockptr;
-		unsigned int pkgsize = 1024;
-		float start = 2.0;
-		// APP = UR, TCP, 1024, 2.0
-		// TCP flow
 		sockptr = ns3::Socket::CreateSocket(leftleaves.Get(i), ns3::TcpSocketFactory::GetTypeId());
 		ns3::Ptr<ns3::TcpSocket> tcpsockptr = ns3::DynamicCast<ns3::TcpSocket> (sockptr);
-		tcpsockptr->SetAttribute("SegmentSize", ns3::UintegerValue(pkgsize));
+		tcpsockptr->SetAttribute("SegmentSize", ns3::UintegerValue(packetSize));
 
-		// std::stringstream nodeidss;
-		// nodeidss << leftleaves.Get(i)->GetId();
-		// std::string prefix = "/NodeList/" + nodeidss.str();
+		ns3::Ptr<ValuedApp> app = ns3::CreateObject<ValuedApp> ();;
+		app->Setup(sockptr, ns3::InetSocketAddress(rightleafifs.GetAddress(i), port), packetSize, 1);
+		app->SetStartTime(ns3::Seconds(startTime));
+		leftleaves.Get(i)->AddApplication(app);
 
 		sinkApps.Add(TcpPacketSinkHelper.Install(rightleaves.Get(i)));
-		float stop = 20.0;
-
-		ns3::Ptr<UnlimitedRateApp> app = ns3::CreateObject<UnlimitedRateApp> ();;
-		app->Setup(sockptr, ns3::InetSocketAddress(rightleafifs.GetAddress(i), port), pkgsize);
-		leftleaves.Get(i)->AddApplication(app);
-		app->SetStartTime(ns3::Seconds(start));
-		app->SetStopTime(ns3::Seconds(stop));
 	}
 
-	// TODO VP-1: check header / header
 	sinkApps.Start(ns3::Seconds(0.0));
 
 	// TODO VP-2: populate manually
 	ns3::Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-	bottleNeckLink.EnablePcapAll("VP_", true);
+	bottleNeckLink.EnablePcap("output/pcap/BN_", routers.Get(0)->GetId(), 0);
 
-	float simstop = 20.0;
-	if (simstop > 0.0) {
-		ns3::Simulator::Stop(ns3::Seconds(simstop));
-	}
-
+	ns3::Simulator::Stop(ns3::Seconds(stopTime));
 	ns3::Simulator::Run ();
-
-	std::cerr << "Dumbbell Left Bottleneck NodeID " << routers.Get(0)->GetId() << std::endl;
-	std::cerr << "Dumbbell Right Bottleneck NodeID " << routers.Get(1)->GetId() << std::endl;
-
 	ns3::Simulator::Destroy ();
 	return 0;
 }
