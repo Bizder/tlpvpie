6683d93bda421bbc84fdcf71a8c996e50badaf0d
Bizder <gergely.tokodi@gmail.com>
2018-03-16 17:18:25 +0100

    valued app removed

--- dbat.bat
+++ dbat.bat
@@ -1,6 +1,5 @@
 docker run -d -it ^
 	--mount type=bind,source=d:/Projects/PVPIE-ns3/pvpie/script,target=/home/ns-allinone-3.27/ns-3.27/scratch/ ^
 	--mount type=bind,source=d:/Projects/PVPIE-ns3/pvpie/pvpie,target=/home/ns-allinone-3.27/ns-3.27/src/pvpie ^
-	--mount type=bind,source=d:/Projects/PVPIE-ns3/pvpie/valuedapp,target=/home/ns-allinone-3.27/ns-3.27/src/valuedapp ^
 	--mount type=bind,source=d:/Projects/PVPIE-ns3/output/,target=/home/ns-allinone-3.27/ns-3.27/output/ ^
-	ns3 /bin/bash
+	23571113/nsc /bin/bash
