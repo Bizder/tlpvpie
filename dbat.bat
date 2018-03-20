00d0177416a0306ffffa8210b2577a3e37dce5dc
Bizder <gergely.tokodi@nokia.com>
2018-03-20 11:36:02 +0100

    script renamed to scripts

--- dbat.bat
+++ dbat.bat
@@ -1,5 +1,5 @@
 docker run -d -it ^
-	--mount type=bind,source=d:/Projects/PVPIE-ns3/pvpie/script,target=/home/ns-allinone-3.27/ns-3.27/scratch/ ^
+	--mount type=bind,source=d:/Projects/PVPIE-ns3/pvpie/scripts,target=/home/ns-allinone-3.27/ns-3.27/scratch/ ^
 	--mount type=bind,source=d:/Projects/PVPIE-ns3/pvpie/pvpie,target=/home/ns-allinone-3.27/ns-3.27/src/pvpie ^
 	--mount type=bind,source=d:/Projects/PVPIE-ns3/output/,target=/home/ns-allinone-3.27/ns-3.27/output/ ^
 	23571113/nsc /bin/bash
