ae802f539a964f0dbbbf76887d1ce5954d402699
Tokodi <gergely.tokodi@nokia.com>
2018-11-20 14:16:10 +0100

    add latest plot (still TODO)

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-OUTPUTFOLDER = "d:/Projects/PVPIE-ns3/output/"
+OUTPUTFOLDER = "c:/Users/tokodi/OTHER/pvpie/output/"
 
 OUTPUTFOLDER = OUTPUTFOLDER if OUTPUTFOLDER.endswith('/') else OUTPUTFOLDER + '/'
 
@@ -34,6 +34,7 @@ with open(OUTPUTFOLDER + 'ascii/delay.bn', 'rb') as delay_file:
 
 	plt.plot(xs, ys, linewidth=1, color='r')
 
+	# plt.ylim(0, 500)
 	plt.plot(xs, np.array([20 for i in xrange(len(xs))]), color='b')
 	plt.title('Delay')
 	plt.xlabel('Time(s)')
@@ -41,3 +42,121 @@ with open(OUTPUTFOLDER + 'ascii/delay.bn', 'rb') as delay_file:
 
 	plt.savefig('delay.png')
 	plt.close()
+
+# TV
+with open(OUTPUTFOLDER + 'ascii/tv.bn', 'rb') as delay_file:
+	xs = []
+	ys = []
+	for line in delay_file.read().splitlines():
+		x, y = line.split()
+		xs.append(float(x))
+		ys.append(float(y))
+
+	plt.plot(xs, ys, linewidth=1, color='r')
+	# plt.axis([0, 30, 400000000, 500000000])
+
+	plt.title('Threshold Value')
+	plt.xlabel('Time(s)')
+	plt.ylabel('TV')
+
+	plt.savefig('tv.png')
+	plt.close()
+
+# throughput
+with open(OUTPUTFOLDER + 'ascii/throughput.bn', 'rb') as delay_file:
+	xs = []
+	ys = []
+
+	for line in delay_file.read().splitlines():
+		x, y = line.split()
+		xs.append(float(x))
+		ys.append(float(y))
+
+	# ys = [y / 1000000.0 for y in ys]
+
+	plt.plot(xs, ys, linewidth=1, color='r')
+
+	plt.axis([0, xs[-1], 0, 10])
+
+	# plt.axis([0, xs[-1], 1.24, 1.25])
+	plt.title('Throughput')
+	plt.xlabel('Time(s)')
+	plt.ylabel('Throughput(Mbps)')
+
+	plt.savefig('throughput.png')
+	plt.close()
+
+
+with open(OUTPUTFOLDER + 'ascii/throughput_gold.bn', 'rb') as gold_file, open(OUTPUTFOLDER + 'ascii/throughput_silver.bn', 'rb') as silver_file, open(OUTPUTFOLDER + 'ascii/throughput_background.bn', 'rb') as background_file:
+	xs = []
+	ys = []
+	for line in gold_file.read().splitlines():
+		x, y = line.split()
+		xs.append(float(x))
+		ys.append(float(y))
+
+	plt.plot(xs, ys, linewidth=1, color='y')
+
+	xs = []
+	ys = []
+	for line in silver_file.read().splitlines():
+		x, y = line.split()
+		xs.append(float(x))
+		ys.append(float(y))
+
+	plt.plot(xs, ys, linewidth=1, color='gray')
+
+	xs = []
+	ys = []
+	for line in background_file.read().splitlines():
+		x, y = line.split()
+		xs.append(float(x))
+		ys.append(float(y))
+
+	plt.plot(xs, ys, linewidth=1, color='b')
+
+	plt.title('Bandwidth share')
+	plt.xlabel('Time(s)')
+	plt.ylabel('Throughput(Mbps)')
+
+	plt.savefig('bws.png')
+	plt.close()
+
+# for i in range(4):
+# 	with open(OUTPUTFOLDER + 'ascii/gold_dr{}.bn'.format(i), 'rb') as delay_file:
+# 		xs = []
+# 		ys = []
+
+# 		for line in delay_file.read().splitlines():
+# 			x, y = line.split()
+# 			xs.append(float(x))
+# 			ys.append(float(y))
+
+# 		ys = [y / 1000000.0 for y in ys]
+
+# 		plt.plot(xs, ys, linewidth=1, color='r')
+
+# 		plt.title('Sending Rate, Gold {}'.format(i))
+# 		plt.xlabel('Time(s)')
+# 		plt.ylabel('DataRate(bps)')
+
+# 		plt.savefig('gold_dr{}.png'.format(i))
+# 		plt.close()
+
+# 	with open(OUTPUTFOLDER + 'ascii/gold_pv{}.bn'.format(i), 'rb') as delay_file:
+# 		xs = []
+# 		ys = []
+
+# 		for line in delay_file.read().splitlines():
+# 			x, y = line.split()
+# 			xs.append(float(x))
+# 			ys.append(float(y))
+
+# 		plt.plot(xs, ys, linewidth=0.1, color='r')
+
+# 		plt.title('Pv graph, Gold {}'.format(i))
+# 		plt.xlabel('Time(s)')
+# 		plt.ylabel('Packet value assigned')
+
+# 		plt.savefig('gold_pv{}.png'.format(i))
+# 		plt.close()
